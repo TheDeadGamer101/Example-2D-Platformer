a9174a019c5cde0cd12ee982396d43d7f4f82505
01-Aether <46935118+TheDeadGamer101@users.noreply.github.com>
2020-11-11 06:25:05 -0800

    player_controller

--- assets/scr/player_controller.gd
+++ assets/scr/player_controller.gd
@@ -1,35 +1,37 @@
 extends KinematicBody2D
 
-export var move_speed : int= 5
-export var accel : int= 100
-export var jump_force : int = 16
+onready var floor_col = $floor_ray
 
+# ints
+export var move_speed : int= 16
+export var accel : int= 1000
+export var jump_force : int = 5
+
+# constants
 const GRAVITY = -20
 
+# vectors
 var move_vec : Vector2= Vector2.ZERO
-
-enum States {IDLE,MOVING,AIR}
-var state = States.IDLE
 
 func _ready():
 	add_to_group("player")
 
 func _process(delta):
-	var hvel = Input.get_action_strength("player_right")-Input.get_action_strength("player_left")
 	
-	match state:
-		States.IDLE:
-			pass
-			
-			if hvel != 0:
-				set_state(States.MOVING)
-		States.MOVING:
-			hvel = hvel * move_speed * accel
-			move_vec.x = hvel
-			if hvel == 0:
-				set_state(States.IDLE)
+	# player jumping nad falling
+	if floor_col.is_colliding():
+		if Input.is_action_just_pressed("player_jump"):
+			move_vec.y -= jump_force * 100
+	else:
+		move_vec.y -= GRAVITY
 	
-	move_vec = move_and_slide(move_vec)
-
-func set_state(state_to):
-	state = state_to
+	# player movement input
+	if Input.is_action_pressed("player_left"):
+		move_vec.x = -move_speed * accel * delta
+	elif Input.is_action_pressed("player_right"):
+		move_vec.x = move_speed * accel * delta
+	else:
+		move_vec.x = lerp(move_vec.x,0,0.5)
+	
+	# player mover
+	move_vec = move_and_slide(move_vec,Vector2.UP)
